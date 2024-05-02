c6ebf93f9bad0c470e61054d3d45ba728e006a66
Guillermo Segura <correo@seguraguillermo.net>
2024-05-02 11:10:36 +0100

    chore: add build script to container

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js' // Content hash is added to handle cache issues
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        // dashboard: `dashboard@${domain}/dashboard/remoteEntry.js`,
+        // auth: `auth@${domain}/auth/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
