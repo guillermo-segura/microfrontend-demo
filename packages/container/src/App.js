dd6d70dfe4ae291f5fc4d723f490260ea23d234e
Guillermo Segura <correo@seguraguillermo.net>
2024-05-13 13:47:13 +0100

    feat: updated header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Container</h1>
+      <h1>App</h1>
       <hr />
       <MarketingApp />
     </div>
