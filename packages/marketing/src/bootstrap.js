af0e2c311171041a4689950cf961d73f6cf3a995
Guillermo Segura <correo@seguraguillermo.net>
2024-05-14 17:49:22 +0100

    feat: add app route

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (element) => {
-  ReactDOM.render(<App />, element);
+const mount = (element, options) => {
+  const history = options.defaultHistory || createMemoryHistory();
+
+  if (options?.onNavigate) {
+    history.listen(options.onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      if (history.location.pathname !== nextPathname)
+      history.push(nextPathname);
+    },
+  };
 };
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
