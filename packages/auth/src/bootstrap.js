bf508391386947557eff6804288a6ba8b54c4e07
Guillermo Segura <correo@seguraguillermo.net>
2024-05-15 10:42:04 +0100

    feat: added auth module

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+import App from './App';
+
+const mount = (element, options) => {
+  const history = options.defaultHistory || createMemoryHistory();
+
+  if (options?.onNavigate) {
+    history.listen(options.onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      if (history.location.pathname !== nextPathname)
+      history.push(nextPathname);
+    },
+  };
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
